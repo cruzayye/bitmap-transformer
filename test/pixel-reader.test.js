60f84ea8b8ce35ac34e0ae7f1875b1f53a0587fa
Aaron Dennis <AMDennis1987@gmail.com>
2019-01-11 11:57:19 -0800

    added test expects and buffer tests

--- test/pixel-reader.test.js
+++ test/pixel-reader.test.js
@@ -1,51 +1,79 @@
-const EventEmitter = require('events');
 const PixelReader = require('../lib/pixel-reader');
 
 describe('Pixel Reader', () => {
 
-    it('reads pixel from buffer', done => {
-        const colors = [];
-        const reader = new PixelReader({ bitsPerPixel: 24 });
-        const ee = new EventEmitter();
-        ee.on('color', (r, g, b) => {
-           rgb = {
-               offset: 'something',
-               r: 90,
-               b: 78,
-               g: 70
-           }
-           console.log('WTF',rgb);
-           colors.push(rbg)
-
-        })
-
-
-        
-        
-
-        // TODO: subscribe to reader "color" event and push into `colors` array.
-        // A "color" object should look like:
-        // {
-        //     offset: <offset from the start of buffer passed to PixelReader>,
-        //     r: <red color value>,
-        //     g: <green color value>,
-        //     b: <blue color value>,
-        // }
-
-        reader.on('end', () => {
-            // write deepEqual assertion for colors versus the
-            // expected rgb color objects
-
-            // Don't forget to call done()!
-        });
-
-        // Create a buffer with known data for your colors
-        const buffer = Buffer.alloc(24 * 3); // for three pixels
-        // TODO: fill buffer with byte values that match your 
-        // expected test colors
-
-        // Call read method with your buffer
-        reader.read(buffer);
+  it('reads pixel from buffer', done => {
+    const colors = [];
+    const reader = new PixelReader({ bitsPerPixel: 24 });
+    reader.on('color', color => {
+      colors.push(color);
     });
+
+    reader.on('end', () => {
+      expect(colors).toHaveLength(3);
+      expect(colors[0]).toEqual({
+        offset: 0,
+        r: 0,
+        g: 0,
+        b: 0
+      });
+
+      expect(colors[1]).toEqual({
+        offset: 3,
+        r: 255,
+        g: 255,
+        b: 255
+      });
+
+      expect(colors[2]).toEqual({
+        offset: 6,
+        r: 0,
+        g: 0,
+        b: 255
+      });
+      done();
+    });
+
+      
+
+
+    
+
+
+    
+    
+
+    // TODO: subscribe to reader "color" event and push into `colors` array.
+    // A "color" object should look like:
+    // {
+    //     offset: <offset from the start of buffer passed to PixelReader>,
+    //     r: <red color value>,
+    //     g: <green color value>,
+    //     b: <blue color value>,
+    // }
+
+    // Create a buffer with known data for your colors
+    const buffer = Buffer.alloc(9); // for three pixels
+    // TODO: fill buffer with byte values that match your 
+    // expected test colors
+    buffer.writeUInt8(0);
+    buffer.writeUInt8(0);
+    buffer.writeUInt8(0);
+
+    // second color white
+    // #FFFFFF
+    buffer.writeUInt8(255);
+    buffer.writeUInt8(255);
+    buffer.writeUInt8(255);
+
+    // third color blue
+    // #FF0000
+    buffer.writeUInt8(255);
+    buffer.writeUInt8(0);
+    buffer.writeUInt8(0);
+
+    // Call read method with your buffer
+    reader.read(buffer);
+  });
 
 });
